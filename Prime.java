24cd65be5c0be56c8a8aaabb687ae8a821d6a2d9
vishnuv1230 <vishnupreetham374@gmail.com>
2024-11-24 00:01:48 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,1 +1,19 @@
+public class Prime {
+    public static void main(String[] args) {
+        int number = 29; // Example number
+        boolean isPrime = true;
 
+        for (int i = 2; i <= number / 2; ++i) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
